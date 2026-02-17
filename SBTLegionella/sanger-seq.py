5b4273e52ad2d0d0508fecf3b227345c19ea15b4
albaag96 <150549354+albaag96@users.noreply.github.com>
2026-02-17 13:04:40 +0100

    Update sanger-seq.py
    actualización codigo

--- SBTLegionella/sanger-seq.py
+++ SBTLegionella/sanger-seq.py
@@ -1,5 +1,12 @@
-from pysanger import * 
-def visualizar_seq(archivo_ab1, template , region="aligned", archivo_salida="output.pdf"):
+import matplotlib
+import numpy
+import Bio
+import pandas
+import logomaker 
+from pysanger import *
+from Bio import SeqIO
+
+def visualizar_seq(archivo_ab1, template , region="all", archivo_salida="output.pdf"):
   abidata = abi_to_dict(archivo_ab1)
   fseq, rseq = generate_consensusseq(abidata)
   fig = visualize(abidata, template=template, region=region)
